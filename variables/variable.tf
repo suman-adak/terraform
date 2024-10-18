555bd54c5781a0329553da39d49c8560f03e8180
suman-adak <sumanadak75@gmail.com>
2024-10-18 12:12:52 +0530

    Create variable.tf

---
+++ variables/variable.tf
@@ -0,0 +1,7 @@
+variable "file" {
+default = "/home/ubuntu/terrform-tutorials/terraform-variables/demo-var.txt"
+}
+
+variable "content" {
+default = "This is coming from a variable which was updated"
+}
